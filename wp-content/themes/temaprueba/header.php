f4d3874a3e5a6b0b0e9a94bfc493bf86f8bf6c47
Nabor <naborlopezjimenez@gmail.com>
2016-11-30 10:52:40 +0000

    Añadido Style.css

--- wp-content/themes/temaprueba/header.php
+++ wp-content/themes/temaprueba/header.php
@@ -1,6 +1,14 @@
 <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri();?>"/>
+    </head>
     <body>
-<a href="https://clasesxe-naborlj.c9users.io/">
-<img src="<?php echo get_template_directory_uri()?>/images/inicio_naranja.png">
+        <div id="header">
+        <h1>Bienvenido a mi WebSite</h1>
+        <div id="icono">
+        <a href="https://clasesxe-naborlj.c9users.io/">
+        <img src="<?php echo get_template_directory_uri()?>/images/icono-inicio.png">
+        </div>
+        </div>
 </a>
 
